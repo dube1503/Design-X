fdf60e331744301e04f5a6f11a04d50223950d55
dube1503 <dube1503@gmail.com>
2018-04-21 13:05:27 -0500

    Pulled action.js from master

--- js/action.js
+++ js/action.js
@@ -1,26 +1,3 @@
-// Image loading animation //
-// $(window).scroll(function() {
-    
-
-//     $(".mix").each(function() {
-//         var top_of_element = $(".grid").offset().top;
-//         var bottom_of_element = $(".grid").offset().top + $(this).outerHeight();
-//         var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
-//         var top_of_screen = $(window).scrollTop();
-//         var offset = $(this).offset();
-//         var curr = $(this);
-
-//         if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
-//             // The element is visible, do something
-//             if (!curr.hasClass("animate")) {
-//                 setTimeout(function(){
-//                     curr.addClass("animate");
-//                 },offset.left);
-//             };
-//         };
-//     });
-// });
-
 // Content filter //
 
 $(document).on("click", "a#filter-btn", function() {
